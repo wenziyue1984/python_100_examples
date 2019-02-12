4f04abe6abf7fd62ca57f85f0d068587cf4535ae
wenziyue <wenziyue1984@163.com>
2019-02-12 11:37:13 +0800

    2019-02-02

--- 008乘法表.py
+++ 008乘法表.py
@@ -10,15 +10,9 @@ def cfb():
 		str_list = []
 		for j in range(1, 10):
 			if j <= i:
-				# print('{} * {} = {}'.format(i, j, i*j))
 				str_list.append('{} * {} = {}'.format(i, j, i*j))
 		print(', '.join(str_list))
 
 if __name__=='__main__':
 	cfb()
 
-
-# for i in range(1, 10):
-#     print 
-#     for j in range(1, i+1):
-#         print "%d*%d=%d" % (i, j, i*j),
