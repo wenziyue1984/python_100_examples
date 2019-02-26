23fc684395ef87a52a45054a87b9920197a82a8d
wenziyue <wenziyue1984@163.com>
2019-02-26 11:27:29 +0800

    2019-02-26

---
+++ 079字符串排序.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+__author__ = 'wenziyue'
+
+'''
+字符串排序。
+'''
+
+if __name__ == '__main__':
+	s1 = input('请输入字符串：')
+	s2 = input('请输入字符串：')
+	s3 = input('请输入字符串：')
+	s_list = [s1, s2, s3]
+	s_new = sorted(s_list)
+	print(s_new)
